8f948ff5416e1136ff361f047079f8db8e76fe22
Mona Lisa <edu.sahan@gmail.com>
2026-02-11 10:43:45 +0530

    create vite project and configure tailwind css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="text-3xl font-bold underline">
+        <h1>National Citizen Issue Resolution System</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
