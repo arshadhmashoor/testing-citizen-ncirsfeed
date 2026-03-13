f52fb050e9df1ea77ef60de68485cbf018d0ecb9
Arshadh <arshadh.mashoor21@gmail.com>
2026-03-13 07:42:52 +0530

    Added the feed  page components and updated frontend structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,43 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Feed from './pages/Feed'
+import Messages from './pages/Messages'
+import ChatBox from './pages/ChatBox'
+import Connections from './pages/Connections'
+import Discover from './pages/Discover'
+import Profile from './pages/Profile'
+import CreatePost from './pages/CreatePost'
+import Dashboard from './pages/Dashboard'
+import Notifications from './pages/Notifications'
+import {useUser} from '@clerk/react'
+import  Layout  from './pages/Layout'
+import {Toaster} from 'react-hot-toast'
 
-function App() {
 
+const App = () => {
+  const {user} = useUser()
   return (
     <>
-      <div className="text-3xl font-bold underline">
-        <h1>National Citizen Issue Resolution System</h1>
-      </div>
+      <Toaster />
+      <Routes>
+        <Route path='/' element={ user ? <Login /> : <Layout />}>
+          <Route index  element={<Feed />} />
+          <Route path='messages'  element={<Messages />} />
+          <Route path='messages/:userId'  element={<ChatBox />} />        
+          <Route path='connections'  element={<Connections />} />        
+          <Route path='discover'  element={<Discover />} />        
+          <Route path='profile'  element={<Profile />} />        
+          <Route path='profile/:profileId'  element={<Profile />} />        
+          <Route path='create-post'  element={<CreatePost />} />        
+          <Route path='notifications'  element={<Notifications />} />        
+          <Route path='dashboard'  element={<Dashboard />} />        
+        </Route>
+      </Routes>
+    
     </>
   )
 }
 
-export default App
+export default App;
+
