783e10ec9420c77de0946d01d7f775c9ee534c00
Arshadh <arshadh.mashoor21@gmail.com>
2026-05-11 13:52:16 +0530

    updated server

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cors());
 
 //create routes
-app.get("/", (req, res) => res.send("Salam personal! Server is running!"));
+app.get("/", (req, res) => res.send(" Server is running!"));
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 4000;
