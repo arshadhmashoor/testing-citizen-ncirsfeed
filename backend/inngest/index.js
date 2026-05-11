a8486fc5ee2e236f1f48a9cc78c0774660c4af96
Arshadh <arshadh.mashoor21@gmail.com>
2026-05-11 08:05:03 +0530

    updated id inngest

--- backend/inngest/index.js
+++ backend/inngest/index.js
@@ -2,7 +2,7 @@ import { Inngest } from "inngest";
 import User from "../models/User.js";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "arshadNCIRS" });
+export const inngest = new Inngest({ id: "arshad-ncirs-bk" });
 
 //inngest functions to save userCitizen data to a database
 const syncUserCreation = inngest.createFunction(
