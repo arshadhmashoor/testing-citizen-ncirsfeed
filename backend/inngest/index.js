45dd4f26ae62171d0ac68f7d58e0f2556f37363a
Arshadh <arshadh.mashoor21@gmail.com>
2026-05-12 12:50:46 +0530

    configure update in vercel, pacake.json1,2 index.js

--- backend/inngest/index.js
+++ backend/inngest/index.js
@@ -2,17 +2,18 @@ import { Inngest } from "inngest";
 import User from "../models/User.js";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "arshad-ncirs-bk" });
+export const inngest = new Inngest({ id: "arshadncirs-be" });
 
 //inngest functions to save userCitizen data to a database
 const syncUserCreation = inngest.createFunction(
-  { id: "sync-user-from-clerk", triggers: { event: "clerk/user.created" } },
+  { id: "sync-user-from-clerk", triggers: [{ event: "clerk/user.created" }] },
 
   async ({ event }) => {
     const { id, first_name, last_name, email_address, image_url } = event.data;
     let username = email_addresses[0].email_address.split("@")[0];
 
     //check username availability
+
     const user = await User.findOne({ username });
 
     if (user) {
@@ -32,7 +33,7 @@ const syncUserCreation = inngest.createFunction(
 
 //inngest function to update userdata in db
 const syncUserUpdation = inngest.createFunction(
-  { id: "update-user-from-clerk", triggers: { event: "clerk/user.updated" } },
+  { id: "update-user-from-clerk", triggers: [{ event: "clerk/user.updated" }] },
 
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } =
@@ -50,7 +51,7 @@ const syncUserUpdation = inngest.createFunction(
 //----------------------------------------------------
 //inngest function to delete userdata in db
 const syncUserDeletion = inngest.createFunction(
-  { id: "delete-user-with-clerk", triggers: { event: "clerk/user.deleted" } },
+  { id: "delete-user-with-clerk", triggers: [{ event: "clerk/user.deleted" }] },
 
   async ({ event }) => {
     const { id } = event.data;
@@ -62,3 +63,5 @@ const syncUserDeletion = inngest.createFunction(
 //----------------------------------------------------------
 // Create an empty array where we'll export future Inngest functions
 export const functions = [syncUserCreation, syncUserUpdation, syncUserDeletion];
+
+//-----------
