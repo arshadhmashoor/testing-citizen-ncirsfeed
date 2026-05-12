fc55f8e57cac06bffe627be37e4779d6343d43bd
Arshadh <arshadh.mashoor21@gmail.com>
2026-05-12 13:50:53 +0530

    configure update in index.js

--- backend/inngest/index.js
+++ backend/inngest/index.js
@@ -10,7 +10,8 @@ const syncUserCreation = inngest.createFunction(
   { id: "sync-user-from-clerk", triggers: [{ event: "clerk/user.created" }] },
 
   async ({ event }) => {
-    const { id, first_name, last_name, email_address, image_url } = event.data;
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
     let username = email_addresses[0].email_address.split("@")[0];
 
     //check username availability
