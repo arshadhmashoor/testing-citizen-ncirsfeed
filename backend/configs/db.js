5d10981de0770ca8633faf3b57b54bf69b5afec5
Arshadh <arshadh.mashoor21@gmail.com>
2026-05-12 11:13:12 +0530

    configure update in db.js

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -2,8 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    mongoose.connection.on("connected", () =>
+      console.log("Databae connected!")
+    );
     await mongoose.connect(process.env.MONGODB_URL);
-    console.log("slm1! Database connected");
   } catch (error) {
     console.log(error.message);
   }
