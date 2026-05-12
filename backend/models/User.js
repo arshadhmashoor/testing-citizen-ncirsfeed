3a64c79a5950c13c42e9169b853bbc2237be3121
Arshadh <arshadh.mashoor21@gmail.com>
2026-05-12 11:21:34 +0530

    configure update in user.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
-const usercSchema = new mongoose.Schema(
+const user_c_Schema = new mongoose.Schema(
   {
     _id: { type: String, required: true },
     email: { type: String, unique: true },
     full_name: { type: String, required: true },
     username: { type: String, unique: true },
-    bio: { type: String, default: "hey, i am using ncirs" },
+    bio: { type: String, default: "let's use ncirs" },
     profile_picture: { type: String, default: "" },
     cover_photo: { type: String, default: "" },
     location: { type: String, default: "" },
@@ -14,6 +14,6 @@ const usercSchema = new mongoose.Schema(
   { timeseries: true, minimize: false }
 );
 
-const User = mongoose.model("User", usercSchema);
+const User = mongoose.model("User", user_c_Schema);
 
 export default User;
