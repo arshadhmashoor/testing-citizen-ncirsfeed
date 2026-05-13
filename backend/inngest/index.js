6d1899d9c8fd3309821242c9a29a848eb65b5cbf
Arshadh <arshadh.mashoor21@gmail.com>
2026-05-13 14:20:21 +0530

    fix clerk user sync function

--- backend/inngest/index.js
+++ backend/inngest/index.js
@@ -12,26 +12,59 @@ const syncUserCreation = inngest.createFunction(
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
-    let username = email_addresses[0].email_address.split("@")[0];
 
-    //check username availability
+    const email = email_addresses?.[0]?.email_address;
 
-    const user = await User.findOne({ username });
+    if (!email) {
+      throw new Error("No email address found from Clerk event");
+    }
 
-    if (user) {
+    let username = email.split("@")[0];
+
+    const existingUser = await User.findOne({ username });
+
+    if (existingUser) {
       username = username + Math.floor(Math.random() * 10000);
     }
 
     const userData = {
       _id: id,
-      email: email_addresses[0].email_address,
+      email,
       full_name: `${first_name || ""} ${last_name || ""}`.trim(),
       profile_picture: image_url,
       username,
     };
+
     await User.create(userData);
   }
 );
+
+// const syncUserCreation = inngest.createFunction(
+//   { id: "sync-user-from-clerk", triggers: [{ event: "clerk/user.created" }] },
+
+//   async ({ event }) => {
+//     const { id, first_name, last_name, email_addresses, image_url } =
+//       event.data;
+//     let username = email_addresses[0].email_address.split("@")[0];
+
+//     //check username availability
+
+//     const user = await User.findOne({ username });
+
+//     if (user) {
+//       username = username + Math.floor(Math.random() * 10000);
+//     }
+
+//     const userData = {
+//       _id: id,
+//       email: email_addresses[0].email_address,
+//       full_name: `${first_name || ""} ${last_name || ""}`.trim(),
+//       profile_picture: image_url,
+//       username,
+//     };
+//     await User.create(userData);
+//   }
+// );
 
 //inngest function to update userdata in db
 const syncUserUpdation = inngest.createFunction(
