a4c6f126881a611557d7304747e9962eaf5689bc
Arshadh <arshadh.mashoor21@gmail.com>
2026-05-16 10:15:43 +0530

    update controller with postContrller.js and models with Post.js

---
+++ backend/controllers/postController.js
@@ -0,0 +1,87 @@
+import fs from "fs";
+import imagekit from "../configs/imageKit.js";
+import Post from "../models/Post.js";
+import CitizenFeed from "../models/CitizenFeed";
+
+//add post issue
+export const addPost = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { content, post_type } = req.body;
+    const images = req.files;
+
+    let image_urls = [];
+
+    if (images.length) {
+      image_urls = await Promise.all(
+        images.map(async (image) => {
+          const fileBuffer = fs.readFileSync(image.path);
+          const response = await imagekit.upload({
+            file: fileBuffer,
+            fileName: image.originalname,
+            folder: "posts",
+          });
+
+          const url = imagekit.url({
+            path: response.filePath,
+            transformation: [
+              { quality: "auto" },
+              { format: "webp" },
+              { width: "1280" },
+            ],
+          });
+          return url;
+        })
+      );
+    }
+    //create post data in db
+    await Post.create({
+      user: userId,
+      content,
+      image_urls,
+      post_type,
+    });
+    res.json({ success: true, message: "issue post created successfully" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//Get post issue
+export const getFeedPosts = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const user = await CitizenFeed.findById(userId);
+
+
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//vote posts/issues
+export const votePost = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const {postId} = req.body
+
+    const post = await Post.findById(postId)
+
+    if(post.votes_count.includes(userId)){
+        post.votes_count = post.votes_count.filter(user=> user !=== userId)
+        await Post.save();
+        res.json({ success: true, message: "Issue unVoted" });
+    }else{
+        post.votes_count.push(userId)
+        await post.save();
+        res.json({ success: true, message: "Issue upVoted" });
+    }
+
+
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
