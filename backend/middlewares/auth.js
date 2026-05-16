5e4e63f57340cded10a40b17465737a4e89b27a2
Arshadh <arshadh.mashoor21@gmail.com>
2026-05-16 11:31:27 +0530

    update auth middleware

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,10 +2,16 @@ export const protect = async (req, res, next) => {
   try {
     const { userId } = await req.auth();
     if (!userId) {
-      return res.json({ success: false, message: "not authenticated" });
+      return res.status(401).json({
+        success: false,
+        message: "not authenticated",
+      });
     }
     next();
   } catch (error) {
-    res.json({ success: false, message: "not authenticated" });
+    return res.status(401).json({
+      success: false,
+      message: "not authenticated",
+    });
   }
 };
