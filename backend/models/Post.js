2ab831dec8499ac4cbac4381213b2b05b4268753
Arshadh <arshadh.mashoor21@gmail.com>
2026-05-16 13:09:39 +0530

    update psot.js

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -10,12 +10,33 @@ const postSchema = new mongoose.Schema(
       enum: ["text", "image", "text_with_image"],
       required: true,
     },
-    location: { type: String, default: "" },
     votes_count: [{ type: String, ref: "CitizenFeed" }],
   },
   { timestamps: true, minimize: false }
 );
 
-const Post = mongoose.model("Post", postSchema);
+const Post = mongoose.models.Post || mongoose.model("Post", postSchema);
 
 export default Post;
+
+// import mongoose from "mongoose";
+
+// const postSchema = new mongoose.Schema(
+//   {
+//     user: { type: String, ref: "CitizenFeed", required: true },
+//     content: { type: String },
+//     image_urls: [{ type: String }],
+//     post_type: {
+//       type: String,
+//       enum: ["text", "image", "text_with_image"],
+//       required: true,
+//     },
+//     location: { type: String, default: "" },
+//     votes_count: [{ type: String, ref: "CitizenFeed" }],
+//   },
+//   { timestamps: true, minimize: false }
+// );
+
+// const Post = mongoose.model("Post", postSchema);
+
+// export default Post;
