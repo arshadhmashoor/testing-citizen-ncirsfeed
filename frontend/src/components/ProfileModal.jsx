7d1c3f3d38283cb478cfc098124249668c3aeafa
Arshadh <arshadh.mashoor21@gmail.com>
2026-05-18 12:35:04 +0530

    updated profileModal.jsx

--- frontend/src/components/ProfileModal.jsx
+++ frontend/src/components/ProfileModal.jsx
@@ -3,9 +3,14 @@ import React, { useState } from "react";
 import { Pencil } from "lucide-react";
 import toast from "react-hot-toast";
 import { Toaster } from "react-hot-toast";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { updateUser } from "../features/citizen/userSlice";
+import { useAuth } from "@clerk/react";
 
 const ProfileModal = ({ setShowEdit }) => {
+  const dispatch = useDispatch();
+  const { getToken } = useAuth();
+
   const user = useSelector((state) => state.user.value);
   const [editForm, setEditForm] = useState({
     username: user.username,
@@ -18,6 +23,26 @@ const ProfileModal = ({ setShowEdit }) => {
 
   const handleSaveProfile = async (e) => {
     e.preventDefault();
+    try {
+      const userData = new FormData();
+    cover_photo: null,
+      const {username, bio, location, profile_picture, cover_photo, full_name } = editForm
+
+      userData.append('username', username)
+      userData.append('bio', bio)
+      userData.append('location', location)
+      userData.append('full_name', full_name)
+      profile_picture && userData.append('profile', profile_picture)
+      cover_photo && userData.append('cover', cover_photo)
+      
+      
+      const token = await getToken();
+      dispatch(updateUser({ userData, token }));
+
+      setShowEdit(false)
+    } catch (error) {
+      toast.error(error.message)
+    }
   };
 
   return (
@@ -26,7 +51,9 @@ const ProfileModal = ({ setShowEdit }) => {
         <div className="bg-white rounded-lg shadow p-6">
           <h1 className="text-2xl font-bold text-gray-900 mb-6">Edit profle</h1>
 
-          <form className="space-y-4" onSubmit={handleSaveProfile}>
+          <form className="space-y-4" onSubmit={e=> toast.promise(
+            handleSaveProfile(e), {loading: 'Saving...'}
+          )}>
             {/*profile picture */}
             <div className="flex flex-col items-start gap-3">
               <label
