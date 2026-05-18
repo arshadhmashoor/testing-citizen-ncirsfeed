5080b4fd358b2f52ca07707d191644936b206a2f
Arshadh <arshadh.mashoor21@gmail.com>
2026-05-18 21:55:21 +0530

    updated storyModal.jsx

--- frontend/src/components/StoriesBar.jsx
+++ frontend/src/components/StoriesBar.jsx
@@ -1,17 +1,35 @@
 import React, { useEffect, useState } from "react";
-import { dummyStoriesData } from "../assets/assets";
+// import { dummyStoriesData } from "../assets/assets";
 import { Plus } from "lucide-react";
 import moment from "moment";
 import StoryModal from "./StoryModal";
 import StoryViewer from "./StoryViewer";
+import api from "../api/axios";
+import toast from "react-hot-toast";
+import { useAuth } from "@clerk/react";
 
 const StoriesBar = () => {
+  const { getToken } = useAuth();
+
   const [stories, setStories] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [viewStory, setViewStory] = useState(null);
 
   const fetchStories = async () => {
-    setStories(dummyStoriesData);
+    // setStories(dummyStoriesData);
+    try {
+      const token = await getToken();
+      const { data } = await api.get("/api/story/get", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      if (data.success) {
+        setStories(data.stories);
+      } else {
+        toast(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   useEffect(() => {
