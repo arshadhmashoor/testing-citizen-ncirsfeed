a3373c704473c13612fc853f34f36bfb7b8434a1
Arshadh <arshadh.mashoor21@gmail.com>
2026-05-18 14:09:44 +0530

    updated feed.jsx

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -2,43 +2,62 @@
 // import { dummyPostsData } from '../assets/assets'
 // import Loading from '../components/Loading'
 
-import { useEffect, useState } from "react"
-import { dummyPostsData } from "../assets/assets"
-import Loading from "../components/Loading"
-import StoriesBar from "../components/StoriesBar"
-import PostCard from "../components/PostCard"
-import { assets } from "../assets/assets"
-import RecentMessages from "../components/RecentMessages"
+import { useEffect, useState } from "react";
+// import { dummyPostsData } from "../assets/assets";
+import Loading from "../components/Loading";
+import StoriesBar from "../components/StoriesBar";
+import PostCard from "../components/PostCard";
+import { assets } from "../assets/assets";
+import RecentMessages from "../components/RecentMessages";
+import { useAuth } from "@clerk/react";
+import api from "../api/axios";
+import toast from "react-hot-toast";
 
 const Feed = () => {
-
-  const [feeds, setFeeds] = useState([])
-  const [loading, setLoading] = useState(true)
+  const [feeds, setFeeds] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { getToken } = useAuth();
 
   const fetchFeeds = async () => {
-    setFeeds(dummyPostsData)
-    setLoading(false)
-  }
+    try {
+      setLoading(true);
+      const { data } = await api.get("/api/post/feed", {
+        headers: {
+          Authorization: `Bearer ${await getToken()}`,
+        },
+      });
+      if (data.success) {
+        setFeeds(data.posts);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+    setLoading(false);
+    // setFeeds(dummyPostsData)
+    // setLoading(false)
+  };
 
-  useEffect(()=>{
-    fetchFeeds()
-  },[])
+  useEffect(() => {
+    fetchFeeds();
+  }, []);
 
   return !loading ? (
-    <div className='h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8'>
+    <div className="h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8">
       {/* Stories and post list */}
       <div>
         {/* <h1>Stories here</h1> */}
         <StoriesBar />
         <div className="p-4 space-y-6">
-          {feeds.map((post)=>(
-            <PostCard key={post._id} post={post}/>
+          {feeds.map((post) => (
+            <PostCard key={post._id} post={post} />
           ))}
         </div>
       </div>
 
       {/* Right Sidebar */}
-      <div className='max-xl:hidden sticky top-0'>
+      <div className="max-xl:hidden sticky top-0">
         {/* <div className='max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow'>
           <h3 className='text-slate-800 font-semibold'>Sponsored</h3>
           <img src={assets.sponsored_img} className='w-75 h-50 rounded-md' alt="" />
@@ -47,9 +66,10 @@ const Feed = () => {
         </div> */}
         <RecentMessages />
       </div>
-
     </div>
-  ) : <Loading />
-}
+  ) : (
+    <Loading />
+  );
+};
 
-export default Feed
+export default Feed;
