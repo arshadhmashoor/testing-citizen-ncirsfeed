df1a169ac067ee75bd71b8f43fae94db08221612
Arshadh <arshadh.mashoor21@gmail.com>
2026-05-18 11:27:10 +0530

    updated layout bak to initial

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -10,36 +10,36 @@ const Layout = () => {
   const user = useSelector((state) => state.user.value);
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
-  // return user ? (
-  //   <div className="w-full flex h-screen">
-  //     <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-
-  //     <div className="flex-1 bg-slate-50">
-  //       <Outlet />
-  //     </div>
-  //     {sidebarOpen ? (
-  //       <X
-  //         className="absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden"
-  //         onClick={() => setSidebarOpen(false)}
-  //       />
-  //     ) : (
-  //       <Menu
-  //         className="absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden"
-  //         onClick={() => setSidebarOpen(true)}
-  //       />
-  //     )}
-  //   </div>
-  // ) : (
-  //   <Loading />
-  // );
-  return (
+  return user ? (
     <div className="w-full flex h-screen">
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
       <div className="flex-1 bg-slate-50">
         <Outlet />
       </div>
+      {sidebarOpen ? (
+        <X
+          className="absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden"
+          onClick={() => setSidebarOpen(false)}
+        />
+      ) : (
+        <Menu
+          className="absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden"
+          onClick={() => setSidebarOpen(true)}
+        />
+      )}
     </div>
+  ) : (
+    <Loading />
   );
+  // return (
+  //   <div className="w-full flex h-screen">
+  //     <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+  //     <div className="flex-1 bg-slate-50">
+  //       <Outlet />
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default Layout;
